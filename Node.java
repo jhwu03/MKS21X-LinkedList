020d42c4cce2ed575b011406d1942a5ccf522934
jwu03 <jwu03@stuy.edu>
2018-12-12 20:59:15 -0500

    added Node class

---
+++ Node.java
@@ -0,0 +1,39 @@
+public class Node{
+  private int data;
+  private Node next,prev;
+
+public Node(int value, Node nxt, Node prv ) {
+    data = value;
+    next = nxt;
+    prev = prv;
+  }
+
+public int getData() {
+    return data;
+  }
+
+  public Node getNext() {
+    return next;
+  }
+
+  public Node getPrev() {
+    return prev;
+  }
+
+  public void setNext(Node newNext) {
+    this.next = newNext;
+  }
+
+  public void setValue(int newValue){
+    this.data = newValue;
+  }
+
+  public void setPrev(Node newPrev) {
+    this.prev = newPrev;
+  }
+
+  public String toString() {
+		return "" + data;
+	}
+
+}
